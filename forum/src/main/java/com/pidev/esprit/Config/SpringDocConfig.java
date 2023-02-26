d4f1881dd499dc6f22f427311c0789dd3e8c8fcb
amineteyeb <mohamedamine.teyeb@esprit.tn>
2023-02-26 22:11:32 +0100

    2nd

--- forum/src/main/java/com/pidev/esprit/Config/SpringDocConfig.java
+++ forum/src/main/java/com/pidev/esprit/Config/SpringDocConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SprigDocConfig {
+public class SpringDocConfig {
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI().info(infoAPI());
